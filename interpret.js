a3ce42c1f79ab4160d2d4890472a2c9682978ebc
vidnath <augustus93@gmail.com>
2015-04-12 17:09:27 -0400

    Changed BNF EQUALS to Eq, and updated interpreter

--- interpret.js
+++ interpret.js
@@ -14,9 +14,19 @@ uxadt._({
 	Div: [_,_],
 	Power: [_,_],
 	Mod: [_,_],
+	True: [],
+	False: [],
+	And: [_,_],
+	Or: [_,_],
+	Not: [_],
+	GT: [_,_],
+	LT: [_,_]
+	GTE: [_,_],
+	LTE: [_,_],
+	Eq: [_,_],
 });
 
-function evaluate(e) {
+function eval_term(e) {
 	if (e._(Number(_))) {
 		[n] = e.matched();
 		return n;
@@ -24,32 +34,83 @@ function evaluate(e) {
 
 	if (e._(Plus(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) + evaluate(e2)
+		return eval_term(e1) + eval_term(e2)
 	}
 
 	if (e._(Minus(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) - evaluate(e2)
+		return eval_term(e1) - eval_term(e2)
 	}
 
 	if (e._(Mult(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) * evaluate(e2)
+		return eval_term(e1) * eval_term(e2)
 	}
 
 	if (e._(Div(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) / evaluate(e2)
+		return eval_term(e1) / eval_term(e2)
 	}
 
 	if (e._(Power(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) ** evaluate(e2)
+		return Math.pow(eval_term(e1), eval_term(e2))
 	}
 
 	if (e._(Mod(_,_))) {
 		[e1, e2] = e.matched();
-		return evaluate(e1) % evaluate(e2)
+		return eval_term(e1) % eval_term(e2)
+	}
+}
+
+function eval_formula(e) {
+	if (e._(True(_))) {
+		[n] = e.matched();
+		return n
 	}
 
+	if (e._(False(_))) {
+		[n] = e.matched();
+		return n
+	}
+
+	if (e._(And(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) && eval_formula(e2)
+	}
+
+	if (e._(Or(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) || eval_formula(e2)
+	}
+
+	if (e._(Not(_))) {
+		[e1] = e.matched();
+		return !eval_formula(e1)
+	}
+
+	if (e._(GT(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) > eval_formula(e2)
+	}
+
+	if (e._(LT(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) < eval_formula(e2)
+	}
+
+	if (e._(GTE(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) >= eval_formula(e2)
+	}
+
+	if (e._(LTE(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) <= eval_term(e2)
+	}
+
+	if (e._(Eq(_,_))) {
+		[e1, e2] = e.matched();
+		return eval_formula(e1) === eval_formula(e2)
+	}
 }
