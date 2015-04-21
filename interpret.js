26cbc65ab0253d4c6bfd668aaa5d7781bd85053b
vidnath <augustus93@gmail.com>
2015-04-21 16:44:06 -0400

    Created execute function

--- interpret.js
+++ interpret.js
@@ -38,16 +38,8 @@ var eval_term = function(env, e) {
                 var e2 = e[key][1];
                 var v1 = eval_term(env,e1);
                 var v2 = eval_term(env,e2);
-                //console.log()
                 return v1 + v2;
             }
-
-            if (key == "Factor") {
-                var e1 = e[key][0];
-                return eval_factor(env, e1);
-            }
-
-            /*
 
             if (key == "Minus") {
                 var e1 = e[key][0];
@@ -57,37 +49,10 @@ var eval_term = function(env, e) {
                 return v1 - v2;
             }
 
-            if (key == "Mult") {
+            if (key == "Factor") {
                 var e1 = e[key][0];
-                var e2 = e[key][1];
-                var v1 = eval_term(env,e1);
-                var v2 = eval_term(env,e2);
-                return v1 * v2;
+                return eval_factor(env, e1);
             }
-
-            if (key == "Div") {
-                var e1 = e[key][0];
-                var e2 = e[key][1];
-                var v1 = eval_term(env,e1);
-                var v2 = eval_term(env,e2);
-                return v1 / v2;
-            }
-
-            if (key == "Mod") {
-                var e1 = e[key][0];
-                var e2 = e[key][1];
-                var v1 = eval_term(env,e1);
-                var v2 = eval_term(env,e2);
-                return v1 % v2;
-            }
-
-            if (key == "Power") {
-                var e1 = e[key][0];
-                var e2 = e[key][1];
-                var v1 = eval_term(env,e1);
-                var v2 = eval_term(env,e2);
-                return Math.pow(v1, v2);
-            } */
         }
     }
 
@@ -107,12 +72,14 @@ var eval_factor = function(env,e) {
             }
 
             if (key == "Variable") {
-                var x = e[key][0];
-                if (env[x] != null) {
-                    return eval_expression(env,env[x]);
-                } else {
-                    alert("Variable not assigned to any value.");
-                }
+                var e1 = e[key][0];
+
+                var o = eval_expression(env,e1);
+                if (o == null) {
+                    alert("Variable is not assigned to any value.");
+                    return "Error: Variable not defined!";
+                } else
+                    return o;
             }
 
             if (key == "Minus") {
@@ -159,23 +126,6 @@ var eval_factor = function(env,e) {
 
     return null;
 };
-
-var exec_assign = function(env,e) {
-    if (typeof e === Node) {
-
-        for (var key in e) {
-            if (key == "Assign") {
-                var vr = e[key][0];
-                var e1 = e[key][1];
-                console.log(e1);
-                //var p = e[key][2];
-                env[vr] = eval_expression(env,e1);
-                console.log(env);
-                return env[vr];
-            }
-        }
-    }
-}
 
 var eval_formula = function(env,e) {
 	if (typeof e === Node) {
@@ -290,178 +240,57 @@ var eval_expression = function(env,e) {
 	return outp;
 };
 
-	/*if (typeof e === Leaf) {
-		for (var key in e) {
 
-			if (key == "True")
-				return true;
-			else
-				return false;
-		}
-	}
-}
+var execute = function(env,e) {
+    //console.log(env);
 
+    if (typeof e === Node) {
 
-*/
+        for (var key in e) {
 
-/*
+            if (key == "Print") {
+                var e1 = e[key][0];
+                var o = eval_expression(env,e1);
+                return o;
+            }
 
-Name: interpret.js
-Author: Vidhu Nath
-Description: This JS file contains the interpreter for the Bridge
-	language and calls upon uxadt.js to operate.
+            if (key == "Assign") {
+                var v = e[key][0];
+                var e1 = e[key][1];
+                var p = e[key][2];
 
+                console.log(e1);
 
-
-// THINGS TO DO STILL::
-//
-// include imports of uxadt.js
-// no direct import allowed in js, might need to include
-// via jquery or through html
-
-// Array prototype extension to use contains function
-Array.prototype.contains = function(obj) {
-    var i = this.length;
-    while (i--) {
-        if (this[i] === obj) {
-            return true;
+                env[v] = e1;
+                console.log(env);
+                var o = execute(env,p);
+                return o;
+            }
         }
     }
-    return false;
 };
 
-//var uxadt = require("./uxadt.js");
-
-// set the initial type
-var _ = null;
-
-// container for the langauge
-uxadt._({
-	Number: [],
-	Variable: [],
-	PLus: [_,_],
-	Minus: [_,_],
-	Mult: [_,_],
-	Div: [_,_],
-	Power: [_,_],
-	Mod: [_,_],
-	True: [],
-	False: [],
-	And: [_,_],
-	Or: [_,_],
-	Not: [_],
-	GT: [_,_],
-	LT: [_,_],
-	GTE: [_,_],
-	LTE: [_,_],
-	Eq: [_,_],
-});
-
-// evaluate the terminals and non-terminals in term
-function eval_term(env,e) {
-	if (e._(Number(_))) {
-		var n = e.match();
-		return n;
-	}
-
-	if (e._(Variable(_))) {
-		var n = e.match();
-
-		if (env.contains(n)) {
-			return eval_term(env, env[n]);
-		} else {
-			alert("Error: Variable is unbounded.");
-		}
-
-	}
-
-	if (e._(Plus(_,_))) {
-		var es = e.match();
-		return eval_term(env, es[0]) + eval_term(env,es[1]);
-	}
-
-	if (e._(Minus(_,_))) {
-		var es = e.match();
-		return eval_term(env, es[0]) - eval_term(env,es[1]);
-	}
-
-	if (e._(Mult(_,_))) {
-		var es = e.match();
-		return eval_term(env, es[0]) * eval_term(env,es[1]);
-	}
-
-	if (e._(Div(_,_))) {
-		var es = e.match();
-		return eval_term(env, es[0]) / eval_term(env,es[1]);
-	}
-
-	if (e._(Power(_,_))) {
-		var es = e.match();
-		return Math.pow(eval_term(env,es[0]), eval_term(env,es[1]));
-	}
-
-	if (e._(Mod(_,_))) {
-		var es = e.match();
-		return eval_term(env, es[0]) % eval_term(env,es[1]);
-	}
-};
 /*
-// evaluate the terminals and non-terminals in formula
-function eval_formula(env,e) {
-	if (e._(True(_))) {
-		[n] = e.match();
-		return n;
-	}
+var exec_assign = function(env,e) {
+    if (typeof e === Node) {
 
-	if (e._(False(_))) {
-		[n] = e.match();
-		return n;
-	}
-
-	if (e._(And(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) && eval_formula(env,e2);
-	}
-
-	if (e._(Or(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) || eval_formula(env,e2);
-	}
-
-	if (e._(Not(_))) {
-		[e1] = e.match();
-		return !eval_formula(env,e1);
-	}
-
-	if (e._(GT(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) > eval_formula(env,e2);
-	}
-
-	if (e._(LT(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) < eval_formula(env,e2);
-	}
-
-	if (e._(GTE(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) >= eval_formula(env,e2);
-	}
-
-	if (e._(LTE(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) <= eval_term(env,e2);
-	}
-
-	// not sure if need "==" or "==="
-	// used "===" by default for comprehensiveness
-	if (e._(Equal(_,_))) {
-		[e1, e2] = e.match();
-		return eval_formula(env,e1) === eval_formula(env,e2);
-	}
+        for (var key in e) {
+            if (key == "Assign") {
+                var vr = e[key][0];
+                var e1 = e[key][1];
+                console.log(e1);
+                //var p = e[key][2];
+                env[vr] = eval_expression(env,e1);
+                console.log(env);
+                return env[vr];
+            }
+        }
+    }
 };
 
-function eval_exp(env,e) {
-	if (e._(Term()))
-}
 */
+
+var interpret = function(env, e) {
+    var outp = execute(env,e);
+    return outp;
+};
